f630b899908603aeb34a576cf41c5065e0f49997
Brandon Baldwin <brandonbaldwin8597@gmail.com>
2025-12-22 02:48:14 -0500

    updates

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Film_Analysis
+# Project: Film_Analysis
+
+## Streamlit application designed to allow users to adjust movie settings to display ideal film suggestions
--- my_app.py
+++ my_app.py
@@ -2,38 +2,52 @@ import streamlit as st
 import pickle
 import pandas as pd
 
-
+# Settings and Title
+st.set_page_config(layout="wide")
 st.sidebar.title('Movie Settings')
 html_temp = """
-<div style="background-color:red;padding:5px">
-<h2 style="color:white;text-align:center;">Recommended Movie Generator</h2>
+<div style="background-color:blue;padding:5px">
+<h2 style="color:white;text-align:center;">Movie Recommendation App</h2>
 </div>"""
-st.markdown(html_temp,unsafe_allow_html=True)
+st.markdown(html_temp, unsafe_allow_html=True)
 
-
+# Sidebar
 length = st.sidebar.radio("What length of movie do you prefer?",('Short', 'Average', 'Long'))
 movie_era = st.sidebar.radio("What movie era do you prefer?",('Vintage', 'Classic', 'Modern'))
 popularity = st.sidebar.radio("Number of Movie Ratings", ('Small Market', 'Main Stream', 'Popular'))
 gross = st.sidebar.pills("Revenue status:", options=['Profitable', 'Unprofitable'])
-genre = st.sidebar.multiselect("Select your favorite genre(s)", ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime',
+
+# Main Page
+genre = st.multiselect("Select your favorite genre(s)", ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime',
        'Drama', 'Family', 'Fantasy', 'Film-Noir', 'History', 'Horror',
        'Music', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Sport',
-       'Thriller', 'War', 'Western'])
+       'Thriller', 'War', 'Western'], max_selections=3)
+
+st.divider()
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Length:", length)
+col2.metric("Era:", movie_era)
+col3.metric("Popularity:", popularity)
+
+st.divider()
+
+colA, colB, colC = st.columns(3)
+colB.button("Show Results", type='primary', use_container_width=True)
+
+# my_dict = {
+#     "Movie Length": length,
+#     "Movie Era": movie_era,
+#     "Popularity": popularity,
+#     'Gross': gross,
+#     "Genre": genre
+# }
+
+# df = pd.DataFrame.from_dict([my_dict])
 
 
-my_dict = {
-    "Movie Length": length,
-    "Movie Era": movie_era,
-    "Popularity": popularity,
-    'Gross': gross,
-    "Genre": genre
-}
-
-df = pd.DataFrame.from_dict([my_dict],)
-
-
-st.header("Your movie settings:")
-st.table(df)
+# st.header("Your movie settings:")
+# st.table(df)
 
 # columns= ['age',
 #  'hp_kW',
